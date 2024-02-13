498083ff1d1c15b2b37edd86091bf32822c0f75f
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-02-13 22:56:33 +0100

    Added basic commands

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 const inputElement = document.getElementById('prompt');
 const terminalElement = document.getElementById('terminal');
+const promptSpanElement = document.getElementById('prompt-span');
+
+cwd = "/"
+directories = 
+    {
+        "home":
+        {
+            "downloads": 
+            {
+                "dog.png":"<IMG DATA>",
+                "cat.png":"<IMG DATA>",
+                "stuff.txt":"dont forget to do laundry"
+            }
+            
+        },
+        "root":
+        {
+            "programs":
+            {
+                "ffmpeg":"<PROGRAM DATA>"
+            },
+            "userdata":
+            {
+                "microsoft":
+                {
+                    "config.ini":"<CONFIG FILE>"
+                }
+            }
+        }
+    }
+
 
 const THEMES = {
     "dark":["#242424","#c1c1c1","#151515","#34ee51"],
@@ -15,23 +46,110 @@ function setTheme(theme){
     r.style.setProperty('--highlightColor', THEMES[theme][3]);
 }
 
-function printOut(text){
+function printOut(text,color="inherit"){
+    if (text.includes("\n")){
+        text = text.split("\n");
+        text.forEach(message => {
+            printOut(message);
+        });
+        return;
+    }
+    // if multiple lines, split to individual messages and print each individually
+
     var textElement = document.createElement("p");
     textElement.innerText = text;
     textElement.classList.add("text-output");
+    textElement.style = "color: "+color;
     terminalElement.insertBefore(textElement, inputElement.parentElement);
 }
 
-function handleCommand(keyword,args){
-    console.log(keyword, args);
-    printOut("received your command!")
+function printError(text){
+    printOut(text,"rgb(255, 118, 118)");
+}
+
+function setPrefix(prefix){
+    promptSpanElement.innerText = prefix;
+    promptSpanElement.appendChild(inputElement);
+}
+
+function directoryExists(path) {
+    const parts = path.split('/').filter(part => part !== '');
+
+    let currentDir = directories;
+
+    for (const part of parts) {
+        if (currentDir[part] && typeof currentDir[part] === 'object') {
+        currentDir = currentDir[part];
+        } else {
+        return false;
+        }
+    }
+
+    return true;
+}
+
+function listDirectory(path) {
+    const parts = path.split('/').filter(part => part !== ''); // Split path into parts and remove empty parts
+  
+    let currentDir = directories;
+  
+    for (const part of parts) {
+      if (currentDir[part] && typeof currentDir[part] === 'object') {
+        currentDir = currentDir[part];
+      } else {
+        return null; // Directory doesn't exist
+      }
+    }
+  
+    return Object.keys(currentDir);
+  }
+
+function handleCommand(raw,keyword,args,argsArray){
+    console.log(keyword, argsArray);
+    printOut(">"+raw);
+    
+    switch (keyword){
+        case "help":
+            printOut("no help 4 u");
+            break;
+        case "pwd":
+            printOut(cwd);
+            break;
+        case "cd":
+            if (argsArray.length != 1){
+                printError("incorrect arguments");
+                break;
+            }
+            var dir = ""
+            if (directoryExists(args)){
+                dir = args;
+            } else if(directoryExists(cwd+args)) {
+                dir = cwd+args;
+            } else {
+                printError("directory '" + args + "' doesn't exist");
+                break;
+            }
+
+            if (!dir.endsWith("/")){
+                dir += "/"
+            }
+
+            cwd = dir;
+            break;
+        case "ls":
+            printOut(listDirectory(cwd).join("\n"));
+            break;
+        default:
+            printError("'"+keyword+"' tis not recognised as an internal command.");
+            break;
+    }
 }
 
 document.body.addEventListener('keydown', function(event) {
     if (event.key === "Enter") {
         let value = inputElement.value;
         let args = value.trim().split(" ");
-        handleCommand(args.shift(),args);
+        handleCommand(value, args.shift(), args.join(" "), args);
         inputElement.value = "";
     };
 });
@@ -43,4 +161,7 @@ function setFocusToPrompt() {
 if (localStorage.getItem("theme") == null){
     localStorage.setItem("theme","dark");
 }
+
+setPrefix(">");
+printOut("ingobeans terminal\n ");
 setTheme("dark");
