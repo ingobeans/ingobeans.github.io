4c9f052dfcc610d36e461ad02711cd097f81c48c
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-10-13 11:42:32 +0200

    added smooth scrolling

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-document.getElementById("overworld").scrollIntoView();
+function smoothScroll(id) {
+  if (navigator.userAgent.toLowerCase().indexOf("firefox") > -1) {
+    // for some reason the built in smooth scrolling doesn't work in firefox for me
+    // manually animate a smooth scroll
+    EPPZScrollTo.scrollVerticalToElementById("overworld", 20);
+  } else {
+    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+window.onload = function () {
+  smoothScroll("overworld");
+};
